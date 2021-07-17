0e4c66ba31d2de0779968ccf67d040619c614095
agui93 <agui93>
2021-07-17 11:39:26 +0800

    update

---
+++ leetcode/0001.findNumberIn2DArray/1.findNumberIn2DArray.go
@@ -0,0 +1,7 @@
+package leetcode
+
+func findNumberIn2DArray(matrix [][]int, target int) bool {
+
+
+	return false
+}
---
+++ leetcode/0001.findNumberIn2DArray/README.md
@@ -0,0 +1,2 @@
+# [1.findNumberIn2DArray] https://leetcode-cn.com/problems/er-wei-shu-zu-zhong-de-cha-zhao-lcof/
+
